5f42500a81ef62ad14a5a8ff1559a5197d0e2104
Biarthon <129562516+Biarthon@users.noreply.github.com>
2025-07-21 00:13:26 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,29 +3,20 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Dinka Family App',
-  description: 'A beautiful app to connect, celebrate, and preserve Dinka heritage with your family.',
-  generator: '', // or your brand name
+  description: 'Developed by Biar',
+  generator: 'Developed by Biar',
   openGraph: {
     title: 'Dinka Family App',
-    description: 'A beautiful app to connect, celebrate, and preserve Dinka heritage with your family.',
+    description: 'Developed by Biar',
     url: 'https://dinkafamilyapp-8zl6.vercel.app',
     siteName: 'Dinka Family App',
-    images: [
-      {
-        url: '/og-image.png', // must be in public folder
-        width: 1200,
-        height: 630,
-        alt: 'Dinka Family App Preview',
-      },
-    ],
     locale: 'en_US',
     type: 'website',
   },
   twitter: {
-    card: 'summary_large_image',
+    card: 'summary',
     title: 'Dinka Family App',
-    description: 'A beautiful app to connect, celebrate, and preserve Dinka heritage.',
-    images: ['/og-image.png'],
+    description: 'Developed by Biar',
   },
 }
 
